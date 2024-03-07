9c947348c1f50d79f1d40649b140fa1497dcc442
Ljoskold <128295295+Ljoskold@users.noreply.github.com>
2024-03-07 14:05:36 +0800

    Initial initiator commit

---
+++ src/initiator.js
@@ -0,0 +1,7 @@
+import css from './styles.css';
+import './displayController';
+import './gameController';
+import './shipsObjects';
+import { Player, player1 } from './player';
+
+console.log(player1.name);
